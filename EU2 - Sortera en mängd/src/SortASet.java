bd64d7103896257bab0ed4b036ee8bb9907f3f7a
WilliamRagstad <41281398+WilliamRagstad@users.noreply.github.com>
2019-11-10 20:09:44 +0100

    Chessboard

--- EU2 - Sortera en mängd/src/SortASet.java
+++ EU2 - Sortera en mängd/src/SortASet.java
@@ -48,6 +48,40 @@ public class SortASet {
 	 *  
 	 *  Utbytessorteringen vs urvalssorteringen
 	 *  ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+	 *  ...
 	 *  
+	 *  Bevis för algoritmen
+	 *  ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+	 *  
+	 *  A) INRE LOOPEN
+	 *  	ETT PÅSTÅENDE OM INRE LOOPEN
+	 *		
+	 *		När den inre loopen har utförts, gäller följande:
+	 *		xi = minimum {xi, xi+1, …, xn}
+	 *		
+	 *		BEVIS
+	 *		
+	 *		För varje iteration i den inre loopen, jämförs alla element
+	 *		efter och från index i+1 med det element på index i.
+	 *		Om de är mindre än det element som för tillfället finns på index i,
+	 *		byter de plats och fortsätter med nästa tal.
+	 *
+	 *		På så sätt kommer alltid det minsta talet ligga på position i efter att den inre
+	 *		loopen har utförts.
+	 *
+	 *  B) HUVUDLOOPEN
+	 *  	ETT PÅSTÅENDE OM HUVUDLOOPEN
+	 *  
+	 *		När huvudloopen har utförts, gäller följande:
+	 *		x1 < x2 < … < xn
+	 *
+	 *		BEVIS
+	 *
+	 *		Om den inre loopen alltid slutar med att det minsta talet
+	 *		av alla tal efter i mängden, ligger på position i, så måste
+	 *		alla tal ligga i storleksordning ifall man utför innerloopen på
+	 *		varje tal index i mängden från vänster till höger (från 0 till mängdens längd)
+	 *		då minsta talet kommer läggas först på index 0, sen näst största på index 1 (minsta
+	 *		av de resesterande talen), osv..
 	 */
 }
